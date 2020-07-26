6a8f8fecaf9d5873aa6baed8ce88abf22463bdd2
Hussam <halshehri@deakin.edu.au>
2020-07-26 22:49:57 +1000

    creat new test file

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require ( 'express' );
+var app = express ();
+app.listen(3000);
+
+// the homepage welecoming massage 
+app . get ( '/' , function ( req , res ) {
+res . send ( ' Welecome page' );
+})
+
+
+//data stored into an array
+let accounts =[
+    { id: 1 , name: 'alex' , deposit: 5 },
+    { id: 2 , name: 'sarah' , deposit: 5 },
+    { id: 3 , name: 'jim' , deposit: 15 }]
+
+
+//retrieve the data by id 
+    app.get ( '/list' , function ( req , res ) {
+        let id=req.query.id;
+        for( let account of accounts ){
+            if (account.id == id){
+                res.json(account)
+            }
+        }
+        })
+
+
+//// convert array to linkedlist 
+
+
+
+
+/// retrieve the data from linkedlis 
+    
+
+
+
+
+        
